12eb245b9db8bdf0c054fd890914a7bac3ccb9ce
mojtaba <mojtabapaso@gmail.com>
2026-07-06 14:45:15 +0330

    migration

---
+++ Order/Order.Infrastructure/Migrations/20260706081810_change-productId-to-guid.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Order.Infrastructure.Persistence.Migrations
+{
+    public partial class changeproductIdtoguid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "BasketItems");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductId",
+                table: "BasketItems",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: Guid.Empty);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "BasketItems");
+
+            migrationBuilder.AddColumn<long>(
+                name: "ProductId",
+                table: "BasketItems",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
